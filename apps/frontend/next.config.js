7984159cfe4a77272cecbbea56a7986e10dc593b
Adinarayana <adiabvs@hotmail.com>
2025-12-10 22:20:53 +0530

    prod deployment instructions

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,8 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
+    domains: [
+      'firebasestorage.googleapis.com',
+      'lh3.googleusercontent.com',
+      'storage.googleapis.com',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+    ],
   },
+  // Production optimizations
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
